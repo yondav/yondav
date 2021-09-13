81bac5efac17ae4dce60eb6ed429be750b1025b0
yondav <yonidavid.abr@gmail.com>
2021-09-12 22:44:30 -0400

    working on pdf render

--- client/src/Components/Resume/index.js
+++ client/src/Components/Resume/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { Document, Page } from 'react-pdf/dist/umd/entry.webpack';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import resume from '../../Assets/yondav-dev-resume-2021.pdf';
-
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 const Resume = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
@@ -16,7 +17,14 @@ const Resume = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 1 }}
     >
-      <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
+      <Document
+        file={resume}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={{
+          cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+          cMapPacked: true,
+        }}
+      >
         <Page
           pageNumber={pageNumber}
           scale={window.innerWidth < 550 ? 0.5 : 1}
