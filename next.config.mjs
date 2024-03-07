a420e03ad7c57f2ac824f974fe8b0d2da3797244
yondav <yoni@yondav.us>
2024-03-07 13:03:33 -0500

    style: formats config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import withTwin from './withTwin.mjs'
+import withTwin from './withTwin.mjs';
 
 /**
  * @type {import('next').NextConfig}
  */
 export default withTwin({
   reactStrictMode: true,
-})
+});
