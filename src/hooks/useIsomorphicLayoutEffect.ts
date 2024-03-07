a923695e7f95bbf807d4705ef52f46a0616539ca
yondav <yoni@yondav.us>
2024-03-07 13:05:20 -0500

    feat(hooks): defines isomorphic layout effect hook

---
+++ src/hooks/useIsomorphicLayoutEffect.ts
@@ -0,0 +1,13 @@
+/**
+ * This custom hook provides an isomorphic version of the 'useLayoutEffect' hook,
+ * which ensures consistent behavior between server and client-side rendering.
+ * @param {Function} effect - The effect function to execute.
+ * @param {Array} deps - (Optional) An array of dependencies for the effect.
+ */
+
+// Import necessary values from React.
+import { useEffect, useLayoutEffect } from 'react';
+
+// Determine whether to use 'useLayoutEffect' or 'useEffect' based on window existence.
+export const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
