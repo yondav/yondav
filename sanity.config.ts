c4e2798eef87a104da8f7ee1d9519877b3bf7e3b
yondav <yoni@yondav.us>
2024-03-11 21:10:54 -0400

    chore: generic sanity cli config and modified sanity config for singleton entry on main page

---
+++ sanity.config.ts
@@ -0,0 +1,46 @@
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
+ */
+
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import { apiVersion, dataset, projectId } from './sanity/env';
+import { schema } from './sanity/schema';
+
+// Define the actions that should be available for singleton documents
+const singletonActions = new Set(['publish', 'discardChanges', 'restore']);
+
+// Define the singleton document types
+const singletonTypes = new Set(['profile']);
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema,
+  plugins: [
+    structureTool({
+      structure: S =>
+        S.list()
+          .title('Content')
+          .items([
+            // Our singleton type has a list item with a custom child
+            S.listItem().title('Profile').id('profile').child(
+              // Instead of rendering a list of documents, we render a single
+              // document, specifying the `documentId` manually to ensure
+              // that we're editing the single instance of the document
+              S.document().schemaType('profile').documentId('profile').title('Profile')
+            ),
+
+            // Regular document types
+          ]),
+    }),
+    // Vision is a tool that lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({ defaultApiVersion: apiVersion }),
+  ],
+});
