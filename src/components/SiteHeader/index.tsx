17931e293575ff05719be079ce257e8f4574cac2
yondav <yoni@yondav.us>
2024-03-12 00:14:56 -0400

    refactor(components): updates neutral color values and implements props for social links

--- src/components/SiteHeader/index.tsx
+++ src/components/SiteHeader/index.tsx
@@ -1,8 +1,9 @@
 import { motion } from 'framer-motion';
-import { useCallback } from 'react';
-import { RiGithubFill, RiLinkedinBoxFill } from 'react-icons/ri';
+import { useCallback, useEffect, useState } from 'react';
 import tw, { styled } from 'twin.macro';
 
+import type { Social } from '../../../sanity/lib';
+import { iconMap } from '../../styles/constants';
 import { Logo } from '../Logo';
 
 const SiteHeaderBase = styled(motion.header).attrs({
@@ -11,44 +12,58 @@ const SiteHeaderBase = styled(motion.header).attrs({
     opacity: 1,
     transition: { duration: 1, delay: 0.2 },
   },
-})(
-  tw`fixed w-screen h-20 py-2 px-4 left-0 top-0 z-50 bg-neutral-200 transition-all flex justify-between items-center`
-);
+})<{ $sticky: boolean }>(({ $sticky }) => [
+  tw`fixed w-screen h-20 py-2 px-4 left-0 top-0 z-50 transition-all duration-700 flex justify-between items-center`,
+  $sticky && tw`bg-neutral-100`,
+]);
 
 const clickableStyle = ({ $translate = true }) => [
-  tw`hover:(brightness-75) cursor-pointer transition-all duration-300`,
+  tw`hover:text-neutral-950 cursor-pointer transition-all duration-300`,
   $translate && tw`hover:(-translate-y-0.5)`,
 ];
 
 const LogoContainer = styled.div(() => [tw`w-12`, clickableStyle({ $translate: false })]);
 
-export function SiteHeader() {
+interface SiteHeaderProps {
+  socials: Social[];
+}
+
+export function SiteHeader({ socials }: SiteHeaderProps) {
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
   const handleScrollTop = useCallback(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }, []);
 
+  const handleSticky = useCallback(() => {
+    const position = window.scrollY;
+    setIsSticky(position > 80);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleSticky, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleSticky);
+    };
+  }, [handleSticky]);
+
   return (
-    <SiteHeaderBase>
+    <SiteHeaderBase $sticky={isSticky}>
       <LogoContainer onClick={handleScrollTop}>
         <Logo />
       </LogoContainer>
       <div css={tw`flex gap-2 text-neutral-800`}>
-        <a
-          href='https://github.com/yondav'
-          target='_blank'
-          rel='noopener noreferrer'
-          css={clickableStyle({ $translate: true })}
-        >
-          <RiGithubFill size='2rem' />
-        </a>
-        <a
-          href='https://www.linkedin.com/in/yondav/'
-          target='_blank'
-          rel='noopener noreferrer'
-          css={clickableStyle({ $translate: true })}
-        >
-          <RiLinkedinBoxFill size='2rem' />
-        </a>
+        {socials.map(s => (
+          <a
+            key={s._key}
+            href={s.link}
+            rel='noopener noreferrer'
+            css={clickableStyle({ $translate: true })}
+          >
+            {iconMap[s.entity]({ size: '2rem' })}
+          </a>
+        ))}
       </div>
     </SiteHeaderBase>
   );
