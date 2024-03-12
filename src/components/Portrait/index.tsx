ffc3fd28e27fe0a4ff0940d4f743303ca7bdabc5
yondav <yoni@yondav.us>
2024-03-12 00:18:23 -0400

    feat(components): implements logo curtain

--- src/components/Portrait/index.tsx
+++ src/components/Portrait/index.tsx
@@ -2,37 +2,29 @@ import { useMemo } from 'react';
 import tw, { styled } from 'twin.macro';
 
 import { ThemeContext } from '../../contexts';
+import { LogoCurtain } from '../Logo';
 
 import { backgrounds } from './Backgrounds';
 import { Attribute } from './Portrait.Attribute';
 import { Canvas } from './Portrait.Canvas';
 import { PortraitMenu } from './Portrait.Menu';
 
-const PlaygroundContainer = styled.div(
-  tw`overflow-hidden border-b-8 border-neutral-50 shadow shadow-neutral-50 flex w-full justify-between relative min-h-[50vh] sm:min-h-[60vh] md:min-h-[calc(100vh - 5rem)]`
-);
+const PlaygroundContainer = styled.div(() => [
+  tw`w-full min-h-[calc(100vh - 5rem)]`,
+  tw`relative flex justify-between`,
+  tw`overflow-hidden md:(shadow shadow-neutral-50)`,
+]);
 
-const BackgroundContainer = styled.div(
-  tw`absolute bottom-0 left-1/2 -translate-x-1/2 min-w-[30rem] sm:min-w-[34rem] md:(min-w-[50rem]) lg:(bottom-1/2 translate-y-1/2) w-full`
-);
+const BackgroundContainer = styled.div(() => [
+  tw`absolute bottom-1/4 left-1/2 -translate-x-1/2`,
+  tw`md:bottom-0 lg:(bottom-1/2 translate-y-1/2)`,
+  tw`min-w-[30rem] sm:min-w-[34rem] md:(min-w-[50rem]) w-full`,
+]);
 
-const PortraitContainer = styled(BackgroundContainer)(
-  tw`bottom-0 translate-y-0 max-w-xs z-20 min-w-0 sm:max-w-md md:(max-w-xl min-w-0) lg:max-w-screen-md`
-);
-
-const PlaygroundMenuContainer = styled.div(
-  tw`w-full flex flex-col gap-4 justify-center items-center bg-neutral-100`
-);
-
-const PlaygroundOptions = styled.div<{ $variant: 'desktop' | 'mobile' }>(
-  ({ $variant }) => [
-    $variant === 'mobile' &&
-      tw`w-full flex flex-wrap gap-4 justify-center items-center md:hidden`,
-
-    $variant === 'desktop' &&
-      tw`hidden md:(relative pt-40 px-8 flex flex-col gap-4 z-50)`,
-  ]
-);
+const PortraitContainer = styled(BackgroundContainer)(() => [
+  tw`bottom-1/4 translate-y-0 max-w-sm z-20 min-w-0`,
+  tw`md:(bottom-0 max-w-xl min-w-0) lg:max-w-screen-md`,
+]);
 
 export function PortraitComponent() {
   const {
@@ -61,6 +53,9 @@ export function PortraitComponent() {
             <Attribute attribute='hat' />
           </Canvas>
         </PortraitContainer>
+        <BackgroundContainer css={tw`absolute z-40`}>
+          <LogoCurtain />
+        </BackgroundContainer>
         <PortraitMenu />
       </PlaygroundContainer>
     </>
