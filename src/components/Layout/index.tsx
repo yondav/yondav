9ef92d89ca77df5b07e0b9349e0b5238bffdbb17
yondav <yoni@yondav.us>
2024-03-12 00:15:34 -0400

    chore(components): implements prop for social links

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,18 @@
 import type { ReactNode } from 'react';
 
+import type { Social } from '../../../sanity/lib';
 import { Container } from '../Container';
 import { SiteHeader } from '../SiteHeader';
 
-export function Layout({ children }: { children: ReactNode }) {
+interface LayoutProps {
+  socials: Social[];
+  children: ReactNode;
+}
+
+export function Layout({ socials, children }: LayoutProps) {
   return (
     <>
-      <SiteHeader />
+      <SiteHeader socials={socials} />
       <Container>{children}</Container>
     </>
   );
